3a56bd1fb083f2c073fe00047b6eeab6cda5b17f
jhzII <jhzll@yandex.ru>
2021-09-08 18:03:57 +0300

    Обработка и занесение данных по файлу.

--- wgred/library/models.py
+++ wgred/library/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class File(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Имя файла')
+
+
+class Word(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Слово')
+
+
+class WordFreq(models.Model):
+    freq = models.IntegerField(verbose_name='Частота')
+    file = models.ForeignKey(File, on_delete=models.CASCADE, verbose_name='Файл')
+    word = models.ForeignKey(Word, on_delete=models.CASCADE, verbose_name='Слово')
--- wgred/library/views.py
+++ wgred/library/views.py
@@ -1,6 +1,45 @@
 from django.shortcuts import render
+import re
+from .forms import UploadFileForm
+from .models import File, Word, WordFreq
 
 
 def index(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            pass
+    else:
+        form = UploadFileForm()
+    return render(request, 'index.html', {'form': form})
 
-    return render(request, 'index.html')
+
+# todo вынести функцию в utils
+def handle_uploaded_file(f):
+    file = File()
+    file.name = f.name
+    file.save()
+
+    for s in f:
+        for w in re.findall(r'[a-zа-я]+', s.decode('utf-8').lower()):
+            word = Word.objects.filter(name=w).first()
+            if not word:
+                word = Word()
+                word.name = w
+                word.save()
+                # todo создать конструктор
+                word_freq = WordFreq()
+                word_freq.freq = 1
+                word_freq.file = file
+                word_freq.word = word
+            else:
+                word_freq = WordFreq.objects.filter(file=file, word=word).first()
+                if word_freq:
+                    word_freq.freq += 1
+                else:
+                    word_freq = WordFreq()
+                    word_freq.freq = 1
+                    word_freq.file = file
+                    word_freq.word = word
+            word_freq.save()
